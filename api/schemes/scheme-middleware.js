20ad988753e864a708f0e03d9902496bf1057dea
Robert Gripshover <robertgrips@gmail.com>
2023-03-23 14:43:13 -0400

    I added the validateStep middlware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -48,7 +48,7 @@ const validateScheme = (req, res, next) => {
     !scheme_name.trim() //<< if we already know it is a string we can 
     //trim it and if it is falsey then it will not move on
     ) {
-
+      next({ status: 400, message: "invalid scheme_name" })
   } else {
     next()
   }
@@ -64,7 +64,21 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body
 
+  if(
+    instructions === undefined || //<< if it is undefined
+    typeof instructions !== 'string' || //<< if the type is not a string
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1 //<< if we already know it is a string we can 
+    //trim it and if it is falsey then it will not move on
+    ) {
+      const error = {status: 400, message: "invalid step"}
+      next(error)
+    } else {
+      next()
+    }
 }
 
 module.exports = {
